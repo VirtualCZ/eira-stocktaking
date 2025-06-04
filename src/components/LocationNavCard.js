915e2ff09d4f40c43392bed5bf31d5651a762dff
NEWVIRTUALPC\VirtualC <tomas.gabriel@sitewell.cz>
2025-06-04 16:10:42 +0200

    fixed builds

--- src/components/LocationNavCard.js
+++ src/components/LocationNavCard.js
@@ -1,53 +1,37 @@
 "use client"
 import Link from "next/link";
 import Card from "./Card";
+import { useEffect, useState } from "react";
 import { useGetLocation } from "@/hooks/useLocation";
 
 export default function LocationNavCard() {
   const getLocation = useGetLocation();
-  const location = getLocation();
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    setLocation(getLocation());
+  }, [getLocation]);
+
   const budova = location?.budova || "-";
   const podlazi = location?.podlazi || "-";
   const mistnost = location?.mistnost || "-";
 
   return (
-    <Card
-      style={{ padding: 0, gap: 0 }}
-    >
+    <Card style={{ padding: 0, gap: 0 }}>
       <div style={{ display: "flex", alignItems: "stretch", width: "100%", minHeight: 96 }}>
         {/* Left: Location info */}
-        <div
-          style={{
-            padding: "1rem",
-            paddingRight: 0,
-            display: "flex",
-            alignItems: "center"
-          }}>
-          <span
-            className="material-icons-round"
-            style={{
-              fontSize: "2rem",
-              color: "#b640ff",
-              borderRadius: "1rem",
-            }}
-          >
+        <div style={{ padding: "1rem", paddingRight: 0, display: "flex", alignItems: "center" }}>
+          <span className="material-icons-round" style={{ fontSize: "2rem", color: "#b640ff", borderRadius: "1rem" }}>
             location_on
           </span>
         </div>
         <div style={{ flex: 1, padding: "1rem", display: "flex", flexDirection: "column", justifyContent: "center" }}>
-          <header
-            style={{
-              color: "#4E5058",
-              fontWeight: 600,
-              fontSize: "1rem",
-              paddingBottom: "0.25rem"
-            }}
-          >
+          <header style={{ color: "#4E5058", fontWeight: 600, fontSize: "1rem", paddingBottom: "0.25rem" }}>
             Lokace
           </header>
-          <div style={{lineHeight: "1.25rem"}}>Budova: {budova}</div>
-          <div style={{lineHeight: "1.25rem"}}>Patro: {podlazi}</div>
-          <div style={{lineHeight: "1.25rem"}}>Místnost: {mistnost}</div>
+          <div style={{ lineHeight: "1.25rem" }}>Budova: {budova}</div>
+          <div style={{ lineHeight: "1.25rem" }}>Patro: {podlazi}</div>
+          <div style={{ lineHeight: "1.25rem" }}>Místnost: {mistnost}</div>
         </div>
         {/* Right: Edit strip */}
         <Link
